12272ccb858942be0bcb049f10414be710ee6a00
Simon_NT <kacsa1984@gmail.com>
2017-04-06 14:09:18 +0200

    feat(open text ready if no arg given)

---
+++ to-do.py
@@ -0,0 +1,11 @@
+import sys
+
+# class Open:
+#
+#     def __init__(self):
+if len(sys.argv) == 1:
+    print("\n" "\n" "Python Todo application \n" "=======================\n" "Command line arguments:\n" "-l   Lists all the tasks\n" "-a   Adds a new task\n" "-r   Removes an task\n" "-c   Completes an task\n" "\n")
+
+
+
+# helpike = Open()
