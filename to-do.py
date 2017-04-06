b6aa2637f116d9a98dea637b3bc6ceb8b6ceb472
Simon_NT <kacsa1984@gmail.com>
2017-04-06 14:39:14 +0200

    feat(open with (-l)line, read the txt file)

--- to-do.py
+++ to-do.py
@@ -1,11 +1,8 @@
 import sys
 
-# class Open:
-#
-#     def __init__(self):
 if len(sys.argv) == 1:
     print("\n" "\n" "Python Todo application \n" "=======================\n" "Command line arguments:\n" "-l   Lists all the tasks\n" "-a   Adds a new task\n" "-r   Removes an task\n" "-c   Completes an task\n" "\n")
-
-
-
-# helpike = Open()
+elif sys.argv[1] == "-l":
+    fr = open("tasks.txt", "r")
+    text = fr.readlines()
+    print(text)
